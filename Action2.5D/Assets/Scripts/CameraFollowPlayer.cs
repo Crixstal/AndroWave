172b7f424fc45e12156f79a6202b0e605b93b211
Leo Lorcet <l.lorcet@student.isartdigital.com>
2021-03-17 15:38:57 +0100

    teleport in background

--- Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
+++ Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
@@ -10,16 +10,38 @@ public class CameraFollowPlayer : MonoBehaviour
     public float rotateSpeed = 0.1f;
     public Vector3 offset = new Vector3(0, 4, -15);
 
+    [HideInInspector]
+    public bool Yaxis = false;
     private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        Vector3 targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
 
     // Update is called once per frame
     void Update()
     {
         Quaternion leftQuaternion = Quaternion.AngleAxis(-rotate, Vector3.up);
         Quaternion rightQuaternion = Quaternion.AngleAxis(rotate, Vector3.up);
+        Vector3 targetPosition;
 
-        Vector3 targetPosition = player.transform.TransformPoint(offset) + new Vector3(1f, 0f, 1f);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        if (!Yaxis)
+        {
+            float y = transform.position.y;
+            targetPosition = player.transform.position + offset;
+            transform.position = new Vector3(targetPosition.x, transform.position.y, transform.position.z);
+        }
+
+        else
+        {
+            //float x = transform.position.x;
+            targetPosition = player.transform.position + offset;
+            transform.position = new Vector3(transform.position.x, targetPosition.y, transform.position.z);
+        }
+
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.rotation = Quaternion.Lerp(transform.rotation, leftQuaternion, rotateSpeed);
