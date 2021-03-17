47eff9c629abd5819cb0b554e254378f0f33bf51
Leo Lorcet <l.lorcet@student.isartdigital.com>
2021-03-17 13:09:57 +0100

    rotate camera

--- Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
+++ Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public float smoothTime = 0.3f;
-    public Vector3 offset = new Vector3(0, 0, -10);
+    public float smoothTime = 0.1f;
+    public float rotate = 5f;
+    public float rotateSpeed = 0.1f;
+    public Vector3 offset = new Vector3(0, 4, -15);
+
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = player.transform.TransformPoint(offset);
+        Quaternion leftQuaternion = Quaternion.AngleAxis(-rotate, Vector3.up);
+        Quaternion rightQuaternion = Quaternion.AngleAxis(rotate, Vector3.up);
+
+        Vector3 targetPosition = player.transform.TransformPoint(offset) + new Vector3(1f, 0f, 1f);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.rotation = Quaternion.Lerp(transform.rotation, leftQuaternion, rotateSpeed);
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.rotation = Quaternion.Lerp(transform.rotation, rightQuaternion, rotateSpeed);
+
+        else
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, rotateSpeed);
     }
 }
