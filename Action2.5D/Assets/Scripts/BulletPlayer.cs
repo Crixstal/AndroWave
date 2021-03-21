ddc4183702463cb9edb169c0809a2770e7d9ba7a
Manon Mehalin <m.mehalin@student.isartdigital.com>
2021-03-21 22:13:59 +0100

    Inputs redone

--- Action2.5D/Assets/Scripts/BulletPlayer.cs
+++ Action2.5D/Assets/Scripts/BulletPlayer.cs
@@ -6,12 +6,23 @@ public class BulletPlayer : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float destructionDelay = 3f;
     [HideInInspector]
     public Vector3 direction;
 
-    void Update()
+    private float startTimer = 0f;
+
+    void FixedUpdate()
     {
         transform.Translate(direction * Time.deltaTime * speed, Space.World);
+
+        startTimer += Time.deltaTime;
+        if (startTimer >= destructionDelay)
+        {
+            Destroy(gameObject);
+            startTimer = 0f;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
--- Action2.5D/Assets/Scripts/MovePlayer.cs
+++ Action2.5D/Assets/Scripts/MovePlayer.cs
@@ -15,13 +15,23 @@ public class MovePlayer : MonoBehaviour
     [SerializeField]
     private float teleportationHeight = 6f;
     [SerializeField]
-    private float teleportationDelay = 6f;
+    private float teleportationDelay = 4f;
     [SerializeField]
     private GameObject bullet = null;
+    [SerializeField]
+    private float angleShoot2D = 50f;
+    [SerializeField]
+    private float angleShoot3D = 5f;
+    [SerializeField]
+    private float horizontalInputSensitivity = 0.5f;
+    [SerializeField]
+    private float verticalInputSensitivity = 0.5f;
 
     private Rigidbody rb;
     private bool isGrounded;
-    private bool verticalAxisIsInUse = false;
+    private bool verticalAxisIsInUse;
+    private bool changeShootPlanIsInUse;
+    private float startTimer = 0f;
 
     void Start()
     {
@@ -33,6 +43,8 @@ public class MovePlayer : MonoBehaviour
         Move();
         Teleport();
         Jump();
+        Visor2D();
+        Visor3D();
         Shoot();
     }
 
@@ -40,9 +52,6 @@ public class MovePlayer : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("HorizontalMovement");
         float verticalInput = Input.GetAxis("Teleport");
-
-        Vector2 inputs = new Vector2(horizontalInput, verticalInput);
-        Debug.Log(inputs);
 
         if (verticalInput == Mathf.Clamp(verticalInput, -0.5f, 0.5f))
         {
@@ -55,43 +64,147 @@ public class MovePlayer : MonoBehaviour
     private void Teleport()
     {
         float verticalInput = Input.GetAxisRaw("Teleport");
+        float rotation = transform.rotation.eulerAngles.y;
 
-        if (verticalInput == 1f)
+        startTimer += Time.deltaTime;
+
+        if (startTimer >= teleportationDelay)
         {
-            if (verticalAxisIsInUse == false)
+            if (verticalInput == 1f)
             {
-                if (transform.position.z == posForeground)
-                    transform.position = new Vector3(transform.position.x, teleportationHeight, posBackground);
+                // ----- TELEPORT BACKGROUND -----
+                if (verticalAxisIsInUse == false && startTimer >= teleportationDelay)
+                {
+                    if (transform.position.z == posForeground)
+                        transform.position = new Vector3(transform.position.x, teleportationHeight, posBackground);
+                    else
+                        return;
 
-                else 
-                    return;
+                    if (rotation == Mathf.Clamp(rotation, 269f, 271f)) // return in 2D shooting
+                        transform.Rotate(0f, 90f, 0f);
 
-                verticalAxisIsInUse = true;
+                    verticalAxisIsInUse = true;
+
+                    startTimer = 0f;
+                }
             }
+
+            else if (verticalInput == -1f)
+            {
+                // ----- TELEPORT FOREGROUND -----
+                if (verticalAxisIsInUse == false && startTimer >= teleportationDelay)
+                {
+                    if (transform.position.z == posBackground)
+                        transform.position = new Vector3(transform.position.x, teleportationHeight, posForeground);
+                    else
+                        return;
+
+                    if (rotation == Mathf.Clamp(rotation, 89f, 91f)) // return in 2D shooting
+                        transform.Rotate(0f, -90f, 0f);
+
+                    verticalAxisIsInUse = true;
+
+                    startTimer = 0f;
+                }
+            }
+
+            else
+                verticalAxisIsInUse = false;
+
         }
 
-        else if (verticalInput == -1f)
-        {
-            if (verticalAxisIsInUse == false)
-            {
-                if (transform.position.z == posBackground)
-                    transform.position = new Vector3(transform.position.x, teleportationHeight, posForeground);
-
-                else
-                    return;
-
-                verticalAxisIsInUse = true;
-            }
-        }
-
-        else
-            verticalAxisIsInUse = false;
+        bullet.transform.position = new Vector3(transform.position.x + transform.localScale.x, transform.position.y, transform.position.z); // bullet same plane as player
     }
 
     private void Jump()
     {
         if (Input.GetButton("Jump") && isGrounded)
-            rb.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jump, ForceMode.Impulse);
+        {
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+    }
+
+    private void Visor2D()
+    {
+        float horizontalInput = Input.GetAxis("HorizontalVisor");
+        float verticalInput = Input.GetAxis("VerticalVisor");
+        float rotation = transform.rotation.eulerAngles.y;
+        float sensitivityRange = Mathf.Clamp(verticalInput, -verticalInputSensitivity, verticalInputSensitivity);
+
+        if (rotation == Mathf.Clamp(rotation, 89f, 91f) || rotation == Mathf.Clamp(rotation, 269f, 271f)) // don't rotate when in 3D shooting
+            return;
+
+        // ----- SHOOT UP -----
+        if (verticalInput > verticalInputSensitivity)
+            bullet.transform.position = new Vector3(transform.position.x, transform.position.y + transform.localScale.y, transform.position.z);
+
+        // ----- SHOOT DOWN -----
+        if (verticalInput < -verticalInputSensitivity)
+            bullet.transform.position = new Vector3(transform.position.x, transform.position.y - transform.localScale.y, transform.position.z);
+
+        // ----- SHOOT RIGHT -----
+        if (rotation != Mathf.Clamp(rotation, -1f, 1f) && verticalInput == sensitivityRange && horizontalInput > horizontalInputSensitivity) // if not already turned
+            transform.Rotate(0f, 180f, 0f);
+        if (rotation == Mathf.Clamp(rotation, -1f, 1f))
+            bullet.transform.position = new Vector3(transform.position.x + transform.localScale.x, transform.position.y, transform.position.z);
+
+        // ----- SHOOT LEFT -----
+        if (rotation != Mathf.Clamp(rotation, 179f, 181f) && verticalInput == sensitivityRange && horizontalInput < -horizontalInputSensitivity) // if not already turned
+            transform.Rotate(0f, 180f, 0f);
+        if (rotation == Mathf.Clamp(rotation, 179f, 181f))
+            bullet.transform.position = new Vector3(transform.position.x - transform.localScale.x, transform.position.y, transform.position.z);
+    }
+
+    private void Visor3D()
+    {
+        float shootInput = Input.GetAxisRaw("Shoot");
+        float horizontalInput = Input.GetAxis("HorizontalVisor");
+        float verticalInput = Input.GetAxis("VerticalVisor");
+        float rotation = transform.rotation.eulerAngles.y;
+
+        if (shootInput == -1f)
+        {
+            if (changeShootPlanIsInUse == false)
+            {
+                // ----- SHOOT BACKGROUND -----
+                if (transform.position.z == posForeground)
+                {
+                    if (rotation == Mathf.Clamp(rotation, -1f, 1f))
+                        transform.Rotate(0f, -90f, 0f);
+
+                    else if (rotation == Mathf.Clamp(rotation, 179f, 181f))
+                        transform.Rotate(0f, 90f, 0f);
+
+                    else if (rotation == Mathf.Clamp(rotation, 269f, 271f))
+                        transform.Rotate(0f, 90f, 0f);
+
+                    bullet.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + transform.localScale.z);
+                    
+                    changeShootPlanIsInUse = true;
+                }
+
+                // ----- SHOOT FOREGROUND -----
+                if (transform.position.z == posBackground)
+                {
+                    if (rotation == Mathf.Clamp(rotation, -1f, 1f))
+                        transform.Rotate(0f, 90f, 0f);
+
+                    else if (rotation == Mathf.Clamp(rotation, 179f, 181f))
+                        transform.Rotate(0f, -90f, 0f);
+
+                    else if (rotation == Mathf.Clamp(rotation, 89f, 91f))
+                        transform.Rotate(0f, -90f, 0f);
+
+                    bullet.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - transform.localScale.z);
+                
+                    changeShootPlanIsInUse = true;
+                }
+            }
+        }
+
+        else
+            changeShootPlanIsInUse = false;
     }
 
     private void Shoot()
@@ -100,35 +213,33 @@ public class MovePlayer : MonoBehaviour
         float horizontalInput = Input.GetAxis("HorizontalVisor");
         float verticalInput = Input.GetAxis("VerticalVisor");
         float rotation = transform.rotation.eulerAngles.y;
+        float sensitivityRange = Mathf.Clamp(verticalInput, -verticalInputSensitivity, verticalInputSensitivity);
 
         if (shootInput == 1f)
         {
+            Instantiate(bullet);
 
-                if (rotation == Mathf.Clamp(rotation, 179f, 181f)) // shoot left
-                    bullet.transform.position = new Vector3(transform.position.x - transform.localScale.x, transform.position.y, transform.position.z);
+            if (rotation != Mathf.Clamp(rotation, 89f, 91f) || rotation != Mathf.Clamp(rotation, 269f, 271f))
+            {
+                if (verticalInput > verticalInputSensitivity) // shoot up
+                    bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(0f, transform.position.y, 0f));
 
-                else if (rotation == Mathf.Clamp(rotation, 269f, 271f)) // shoot background
-                    bullet.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + transform.localScale.z);
-
-                else if (rotation == Mathf.Clamp(rotation, 89f, 91f)) // shoot foreground
-                    return;
-
-                else // shoot right
-                    bullet.transform.position = new Vector3(transform.position.x + transform.localScale.x, transform.position.y, transform.position.z);
-
-                Instantiate(bullet);
-
-
-            if (rotation == Mathf.Clamp(rotation, 179f, 181f)) // shoot left
-                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(-transform.position.x, verticalInput * 10f, 0f));
+                else if (verticalInput < -verticalInputSensitivity && !isGrounded) // shoot down
+                    bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(0f, -transform.position.y, 0f));
+            }
             
+            if (rotation == Mathf.Clamp(rotation, -1f, 1f) && verticalInput == sensitivityRange) // shoot right
+                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(transform.position.x, verticalInput * angleShoot2D, 0f));
+
+            else if (rotation == Mathf.Clamp(rotation, 179f, 181f) && verticalInput == sensitivityRange) // shoot left
+                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(-transform.position.x, verticalInput * angleShoot2D, 0f));
+
+            if (rotation == Mathf.Clamp(rotation, 89f, 91f)) // shoot foreground
+                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(horizontalInput * angleShoot3D, verticalInput * angleShoot3D, -transform.position.z));
+
             else if (rotation == Mathf.Clamp(rotation, 269f, 271f)) // shoot background
-                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(horizontalInput * 10f, 0f, transform.position.z));
-
-            else // shoot right
-                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(transform.position.x, verticalInput * 10f, 0f));
+                bullet.GetComponent<BulletPlayer>().direction = Vector3.Normalize(new Vector3(horizontalInput * angleShoot3D, verticalInput * angleShoot3D, transform.position.z));
         }
-
     }
 
     private void OnCollisionStay(Collision collision)
