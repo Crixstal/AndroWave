f764b07f3fb339337e1b16579f77a14c6bf50eab
Leo Lorcet <l.lorcet@student.isartdigital.com>
2021-03-24 16:03:52 +0100

    player not at the center of camera when moving

--- Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
+++ Action2.5D/Assets/Scripts/CameraFollowPlayer.cs
@@ -7,7 +7,7 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField]
     private GameObject player = null;
     [SerializeField]
-    private float smoothTime = 0.1f, rotate = 5f, rotateSpeed = 0.1f;
+    private float smoothTime = 0.1f, cameraShift = 2f;
 
     [HideInInspector]
     public bool Yaxis = false;
@@ -15,17 +15,16 @@ public class CameraFollowPlayer : MonoBehaviour
     public Vector3 offset = new Vector3(0, 4, -15);
     private Vector3 velocity = Vector3.zero;
     private float foregroundZ;
+    private float yPos;
 
     private void Start()
     {
         foregroundZ = player.GetComponent<Player>().posForeground;
+        yPos = transform.position.y;
     }
 
     void Update()
     {
-        Quaternion startQuaternion = new Quaternion(transform.rotation.x, 0, 0, transform.rotation.w);
-        Quaternion leftQuaternion = Quaternion.AngleAxis(-rotate, Vector3.up) * startQuaternion;
-        Quaternion rightQuaternion = Quaternion.AngleAxis(rotate, Vector3.up) * startQuaternion;
         Vector3 targetPosition;
 
         if (!Yaxis)
@@ -40,18 +39,23 @@ public class CameraFollowPlayer : MonoBehaviour
             transform.position = new Vector3(transform.position.x, targetPosition.y, transform.position.z);
         }
 
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetPosition.x, targetPosition.y, foregroundZ + offset.z), ref velocity, smoothTime);
+        transform.position = new Vector3(targetPosition.x, yPos + offset.y, foregroundZ + offset.z);
 
+        Vector3 leftShift = transform.position + new Vector3(cameraShift, 0, 0);
+        Vector3 rightShift = transform.position + new Vector3(-cameraShift, 0, 0);
+        Vector3 baseCamera = transform.position;
         float horizontalInput = Input.GetAxis("HorizontalMovement");
         float verticalInput = Input.GetAxis("Teleport");
 
         if (horizontalInput < 0 && verticalInput == Mathf.Clamp(verticalInput, -0.4f, 0.4f))
-            transform.rotation = Quaternion.Lerp(transform.rotation, leftQuaternion, rotateSpeed);
+            targetPosition = rightShift;
 
         else if (horizontalInput > 0 && verticalInput == Mathf.Clamp(verticalInput, -0.4f, 0.4f))
-            transform.rotation = Quaternion.Lerp(transform.rotation, rightQuaternion, rotateSpeed);
+            targetPosition = leftShift;
 
         else
-            transform.rotation = Quaternion.Lerp(transform.rotation, startQuaternion, rotateSpeed);
+            targetPosition = baseCamera;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
