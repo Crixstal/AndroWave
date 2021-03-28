8389d278065adaef859f31d333710f094421555e
Manon Mehalin <m.mehalin@student.isartdigital.com>
2021-03-28 15:00:53 +0200

    Railgun in progress + change player teleportation system

--- Action2.5D/Assets/Scripts/Weapons/MachineGun.cs
+++ Action2.5D/Assets/Scripts/Weapons/MachineGun.cs
@@ -15,28 +15,28 @@ public class MachineGun : WeaponPlayer
             // ---------- SHOOT UP ----------
             if (verticalInput > verticalInputSensitivity)
             {
-                GameObject currentBullet = Instantiate(bullet, new Vector3(Random.Range(playerPos.x - spawnRange, playerPos.x + spawnRange), weaponPos.y + weaponLength, weaponPos.z), Quaternion.Euler(0f, 0f, 90f));
+                GameObject currentBullet = Instantiate(bullet, new Vector3(Random.Range(playerPos.x - spawnRange, playerPos.x + spawnRange), weaponPos.y + weaponLength, playerPos.z), Quaternion.Euler(0f, 0f, 90f));
                 currentBullet.GetComponent<BulletPlayer>().direction = Vector3.up;
             }
             
             // ---------- SHOOT DOWN ----------
             else if (verticalInput < -verticalInputSensitivity && canShoot)
             {
-                GameObject currentBullet = Instantiate(bullet, new Vector3(Random.Range(playerPos.x - spawnRange, playerPos.x + spawnRange), weaponPos.y - weaponLength, weaponPos.z), Quaternion.Euler(0f, 0f, -90f));
+                GameObject currentBullet = Instantiate(bullet, new Vector3(Random.Range(playerPos.x - spawnRange, playerPos.x + spawnRange), weaponPos.y - weaponLength, playerPos.z), Quaternion.Euler(0f, 0f, -90f));
                 currentBullet.GetComponent<BulletPlayer>().direction = Vector3.down;
             }
             
             // ---------- SHOOT RIGHT ----------
             if (playerRot == Mathf.Clamp(playerRot, -1f, 1f) && verticalInput == isInSensitivityRange)
             {
-                GameObject currentBullet = Instantiate(bullet, new Vector3(weaponPos.x + weaponLength, Random.Range(weaponPos.y - spawnRange, weaponPos.y + spawnRange), weaponPos.z), Quaternion.Euler(0f, playerRot, 0f));
+                GameObject currentBullet = Instantiate(bullet, new Vector3(weaponPos.x + weaponLength, Random.Range(weaponPos.y - spawnRange, weaponPos.y + spawnRange), playerPos.z), Quaternion.Euler(0f, playerRot, 0f));
                 currentBullet.GetComponent<BulletPlayer>().direction = Vector3.right;
             }
             
             // ---------- SHOOT LEFT ----------
             else if (playerRot == Mathf.Clamp(playerRot, 179f, 181f) && verticalInput == isInSensitivityRange)
             {
-                GameObject currentBullet = Instantiate(bullet, new Vector3(weaponPos.x - weaponLength, Random.Range(weaponPos.y - spawnRange, weaponPos.y + spawnRange), weaponPos.z), Quaternion.Euler(0f, playerRot, 0f));
+                GameObject currentBullet = Instantiate(bullet, new Vector3(weaponPos.x - weaponLength, Random.Range(weaponPos.y - spawnRange, weaponPos.y + spawnRange), playerPos.z), Quaternion.Euler(0f, playerRot, 0f));
                 currentBullet.GetComponent<BulletPlayer>().direction = Vector3.left;
             }
         }
