c975e88f253192a9d41959a99448e541ef016c1e
Gunther Yvan <y.gunther@student.isartdigital.com>
2021-03-30 10:01:23 +0200

    LD

---
+++ Action2.5D/Assets/Enemies Manon/Gorille.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gorille : Enemy
+{
+    public override void Shoot()
+    {
+        if (Time.time > shotTimer)
+        {
+            shotTimer = Time.time + delayPerShot;
+
+            //for (int i = 0; i < bulletPerSalve; ++i)
+            //{
+            GameObject currentBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            currentBullet.GetComponent<BulletEnemy>().direction = Vector3.Normalize(relativePos);
+            //}
+        }
+    }
+}
