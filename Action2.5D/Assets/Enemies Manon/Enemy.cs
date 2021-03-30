c69199be21b5442e1dd042755ca51fae5c2efae4
Leo Lorcet <l.lorcet@student.isartdigital.com>
2021-03-30 15:09:49 +0200

    screen shake and invincibility frames

--- Action2.5D/Assets/Enemies Manon/Enemy.cs
+++ Action2.5D/Assets/Enemies Manon/Enemy.cs
@@ -15,6 +15,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] protected float delayPerShot = 0f;
     //[SerializeField] protected float bulletPerSalve = 0f;
     [SerializeField] protected GameObject playerBullet = null;
+    [SerializeField] private Material material = null;
 
     protected float shotTimer = 0f;
 
@@ -26,16 +27,21 @@ public class Enemy : MonoBehaviour
     protected Vector3 weaponPos = Vector3.zero;
     protected float weaponLength = 0f;
     protected float weaponRot = 0f;
+    private Color baseColor;
 
     void Start()
     {
         bullet.GetComponent<BulletEnemy>().speed = m_speed;
         bullet.GetComponent<BulletEnemy>().damage = m_damage;
         bullet.GetComponent<BulletEnemy>().destructionDelay = m_destructionDelay;
+        baseColor = material.color;
     }
 
     public void FixedUpdate()
     {
+        if (material.color != baseColor)
+            material.color = baseColor;
+
         enemyPos = transform.position;
         enemyRot = transform.rotation.eulerAngles.y;
 
@@ -85,6 +91,9 @@ public class Enemy : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 9) // 9 = BulletPlayer
+        {
             life -= playerBullet.GetComponent<BulletPlayer>().damage;
+            material.color = new Color(255, 255, 255);
+        }
     }
 }
